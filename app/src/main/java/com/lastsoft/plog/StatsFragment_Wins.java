8e64a18cd2850fde4cb46ff5e2290885f1ccaadd
Adam Laster <aelaster@gmail.com>
2015-06-19 10:57:16 -0400

    Update 61 - 0.0.15

--- app/src/main/java/com/lastsoft/plog/StatsFragment_Wins.java
+++ app/src/main/java/com/lastsoft/plog/StatsFragment_Wins.java
@@ -183,11 +183,17 @@ public class StatsFragment_Wins extends Fragment {
                 addStat(6, getString(R.string.stats_total_losses) + ":", loserCounter + "", "");
                 for (int x = 0; x < result.size(); x++) {
                     addStat(-1, result.get(x).player.playerName + " " + getString(R.string.stats_regular_wins) + getString(R.string.percentage) + ":", ((int) (result.get(x).regularWins * 100.0 / result.get(0).totalPlays + 0.5)) + "%", result.get(x).player.getId() + "");
-                    addStat(-1, result.get(x).player.playerName + " " + getString(R.string.stats_asterisk_wins) + getString(R.string.percentage) + ":", ((int) ((result.get(x).asteriskWins) * 100.0 / result.get(0).totalPlays + 0.5)) + "%", result.get(x).player.getId() + "");
+                    if (result.get(x).asteriskWins > 0) {
+                        addStat(-1, result.get(x).player.playerName + " " + getString(R.string.stats_asterisk_wins) + getString(R.string.percentage) + ":", ((int) ((result.get(x).asteriskWins) * 100.0 / result.get(0).totalPlays + 0.5)) + "%", result.get(x).player.getId() + "");
+                    }
                     addStat(-1, result.get(x).player.playerName + " " + getString(R.string.stats_total_wins) + getString(R.string.percentage) + ":", ((int) ((result.get(x).asteriskWins + result.get(x).regularWins) * 100.0 / result.get(0).totalPlays + 0.5)) + "%", result.get(x).player.getId() + "");
                 }
-                addStat(-1, getString(R.string.stats_shared_wins) + getString(R.string.percentage) + ":", ((int) (sharedCounter * 100.0 / result.get(0).totalPlays + 0.5)) + "%", "");
-                addStat(-1, getString(R.string.stats_total_losses) + getString(R.string.percentage) + ":", ((int) (loserCounter * 100.0 / result.get(0).totalPlays + 0.5)) + "%", "");
+                if (sharedCounter > 0) {
+                    addStat(-1, getString(R.string.stats_shared_wins) + getString(R.string.percentage) + ":", ((int) (sharedCounter * 100.0 / result.get(0).totalPlays + 0.5)) + "%", "");
+                }
+                if (loserCounter > 0) {
+                    addStat(-1, getString(R.string.stats_total_losses) + getString(R.string.percentage) + ":", ((int) (loserCounter * 100.0 / result.get(0).totalPlays + 0.5)) + "%", "");
+                }
             }
             mydialog.dismiss();
         }
